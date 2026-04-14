5e5141003497f4d34a112f937ffb62231146d46a
Vipul <ptlvipul774@gmail.com>
2026-04-14 14:24:08 +0530

    removed exe file

--- while_loop.cpp
+++ while_loop.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int i=5;
-    while(i<10)
-    {
-        cout << i <<" ";
+
+int main() {
+    int i = 1;
+
+    while(i <= 5) {
+        cout << i << " ";
         i++;
     }
+
     return 0;
-    
 }
