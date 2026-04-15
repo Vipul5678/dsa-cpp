e1670401059a7365f4f63d34e7abd80113cdc61c
Vipul <ptlvipul774@gmail.com>
2026-04-15 09:37:57 +0530

    nested program

---
+++ for_nested.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=2; i<=6; i++){
+        for(int j=2; j<=4; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
