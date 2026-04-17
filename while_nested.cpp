c3544bb3bd2bf4047191c7181774ae1ce79ce2ff
Vipul <ptlvipul774@gmail.com>
2026-04-17 12:41:05 +0530

    while nested loop

---
+++ while_nested.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while (i<=4)
+    {
+        int j=1;
+        while (j<=5)
+        {
+            cout<<"*"<<" ";
+            j++;
+        }
+
+        cout<<endl;
+        i++;
+        
+    }
+    
+}
